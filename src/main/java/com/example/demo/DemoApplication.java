b0fdf391ab41b1195dd8befddbec2d01d754f28f
jongwon <savior371@gmail.com>
2021-01-03 20:01:08 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        String comment = "Hello World!";
+        System.out.print(comment);
+    }
+}
